d7113daa38cde7c02805ff3149da6e6954f90420
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-26 14:10:29 -0400

    Finished model files

--- models/Product.js
+++ models/Product.js
@@ -16,20 +16,32 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "product",
-        key: "id"
-      }
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "tag",
-        key: "id"
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isNumeric: true
+        }
+    },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true
+        }
+    },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "category",
+          key: "id"
+        }
       }
-    }
   },
   {
     sequelize,
