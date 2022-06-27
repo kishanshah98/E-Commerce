c3295295c0d20e30164f38f72dd189488ae51f5d
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-26 22:18:53 -0400

    Finished creating models and finished importing sequalize and sync sequalize in server file

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -20,28 +20,28 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-      price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        validate: {
-          isNumeric: true
-        }
-    },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10,
-        validate: {
-          isNumeric: true
-        }
-    },
-      category_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "category",
-          key: "id"
-        }
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true
       }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -17,12 +17,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: "tag_id"
+  through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: "tag_id"
+  through: ProductTag
 })
 
 module.exports = {
